1d38043043b4a94c1766405e88cc5c985a9feeff
ta-ka-13 <edll5505d@yahoo.co.jp>
2020-07-02 18:44:51 +0900

    show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
 
   devise_for :users
 root 'shares#index'
+get 'users/:id' => 'users#show' 
+
 
 resources :users
 resources :shares
+
+get 'users/:id' => 'users#show' 
+
+# resources :users, only: [:show]
+
 end
