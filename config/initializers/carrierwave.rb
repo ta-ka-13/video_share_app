1a1be2f1bfa861b69a942906ae5e24599661336c
ta-ka-13 <edll5505d@yahoo.co.jp>
2020-07-10 04:31:42 +0900

    carrierwave_asset_host

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'video-share-app-t'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/video-share-app-t'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com'
 end
