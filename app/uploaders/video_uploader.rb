ff17ed73488357b7b3941a817b55e790e6c303ee
ta-ka-13 <edll5505d@yahoo.co.jp>
2020-07-11 19:08:59 +0900

    uploder

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -34,25 +34,25 @@ class VideoUploader < CarrierWave::Uploader::Base
   # end
 
 
-  # version :screenshot do
-  #   process :screenshot
-  #   def full_filename (for_file = model.logo.file)
-  #     "screenshot.jpg"
-  #   end
-  # end
+  version :screenshot do
+    process :screenshot
+    def full_filename (for_file = model.logo.file)
+      "screenshot.jpg"
+    end
+  end
 
-  # def screenshot
-  #   tmpfile = File.join(File.dirname(current_path), "tmpfile")
+  def screenshot
+    tmpfile = File.join(File.dirname(current_path), "tmpfile")
 
-  #   File.rename(current_path, tmpfile)
+    File.rename(current_path, tmpfile)
 
-  #   movie = FFMPEG::Movie.new(tmpfile)
-  #   movie.screenshot(current_path + ".jpg", {resolution: '512x312' }, preserve_aspect_ratio: :width)
+    movie = FFMPEG::Movie.new(tmpfile)
+    movie.screenshot(current_path + ".jpg", {resolution: '512x312' }, preserve_aspect_ratio: :width)
 
-  #   File.rename(current_path + ".jpg", current_path)
+    File.rename(current_path + ".jpg", current_path)
 
-  #   File.delete(tmpfile)
-  # end
+    File.delete(tmpfile)
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
